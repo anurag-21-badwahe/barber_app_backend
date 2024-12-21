7e53fa1cf8d11fcacc1343568695a2014ee218a3
anurag <anuragbadwahe@gmail.com>
2024-12-22 00:40:50 +0530

    Created Auth Middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,1 +1,67 @@
 const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  let token;
+  const authHeader = req.headers.Authorization || req.headers.authorization;
+  
+
+  
+
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.status(401).json({
+      success: false,
+      message: "Authorization token is missing",
+    });
+  }
+
+  try {
+    // Split by space instead of empty string and get the token
+    token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Token is missing in Authorization header",
+      });
+    }
+
+    // Verify the token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    
+    
+    // Add decoded user to request object
+    req.user = decoded;
+    // console.log("Decoded User",req.user);
+
+    // Check token expiration
+    if (decoded.exp < Date.now() / 1000) {
+      return res.status(401).json({
+        success: false,
+        message: "Token has expired",
+      });
+    }
+
+    next();
+  } catch (error) {
+    if (error.name === "JsonWebTokenError") {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token",
+      });
+    }
+    if (error.name === "TokenExpiredError") {
+      return res.status(401).json({
+        success: false,
+        message: "Token has expired",
+      });
+    }
+
+    return res.status(500).json({
+      success: false,
+      message: "Failed to authenticate token",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = verifyToken;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,19 +1,20 @@
 const express = require("express");
+const verifyToken = require('../middleware/authMiddleware')
 
 const router  = express.Router();
 
 // Only admin can access this route
-router.get("/admin",(req,res)=>{
+router.get("/admin",verifyToken,(req,res)=>{
     res.json({message : "Welcome Admin"})
 })
 
 // Only Customer can access this route
-router.get("/customer",(req,res)=>{
+router.get("/customer",verifyToken,(req,res)=>{
     res.json({message : "Welcome Customer"})
 })
 
 // Only Salon can access this route
-router.get("/salon",(req,res)=>{
+router.get("/salon",verifyToken,(req,res)=>{
     res.json({message : "Welcome Salon"})
 })
 
